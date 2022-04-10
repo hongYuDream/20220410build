83f9755e9f44828f15cdbabe22946499f2b62361
wuhailong <wu18220018843@163.com>
2022-04-10 17:04:46 +0800

    添加family主工程

---
+++ family-honor-wall/src/main/java/com/hailong/familyhonorwall/FamilyHonorWallApplication.java
@@ -0,0 +1,13 @@
+package com.hailong.familyhonorwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyHonorWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyHonorWallApplication.class, args);
+    }
+
+}
---
+++ family-honor-wall/src/test/java/com/hailong/familyhonorwall/FamilyHonorWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hailong.familyhonorwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyHonorWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
